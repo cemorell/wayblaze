77a0ff2e55dd49f9ef30005c0993de72235ed935
Max DePriest <maxd924@gmail.com>
2016-07-09 23:03:08 -0500

    added form and show pages to views

--- routes/index.js
+++ routes/index.js
@@ -22,34 +22,14 @@ router.get('/api/search', function(req, res, next) {
   		//return body;
     //it works!
   });
+});
 
+router.get('/form', function (req, res, next) {
+	res.render('form');
+});
 
-// 	var options = {
-// 	  host: 'api.wayblazer.com',
-// 	  port: 443,
-// 	  path: '/v1/accommodations/seearch?adults=1&children=2&rooms=1&startDate=2016-10-06&endDate=2016-10-10&getPricing=true',
-// 	  method: 'GET',
-// 	  headers: {
-// 	    'x-api-key': 'GUMpqpsgg96D1y8rcf6dBC32JVWVUnO2XWfMMa7e'
-// 	  }
-// 	};
-
-// 	http.get(options, function(res) {
-// 	  console.log('STATUS: ' + res.statusCode);
-// 	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-// 	  res.setEncoding('utf8');
-// 	  var str = '';
-// 	  res.on('data', function (chunk) {
-// 	    console.log('BODY: ' + chunk);
-// 	    str += chunk;
-// 	  });
-// 	  res.on('end', function () {
-// 	  	console.log(str);
-// 	  })
-// 	}).on("error", function(e) {
-// 		console.log("Got error: " , e);
-// 	})
-// 	.end();
+router.get('/show', function (req, res, next) {
+	res.render('show');
 });
 
 module.exports = router;
